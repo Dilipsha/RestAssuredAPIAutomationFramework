29e0c59617b7f85e76d1368e3e602ab7afb5692d
Dilip.Sharma <sharma.dilip137@gmail.com>
2024-10-09 16:06:30 +0530

    added initial dependencies for framework

--- src/test/java/airlines/AirlinesTest.java
+++ src/test/java/airlines/AirlinesTest.java
@@ -6,18 +6,18 @@ import org.testng.annotations.Test;
 import restUtils.RestUtils;
 import utils.JSonUtils;
 
+import java.io.IOException;
 import java.util.*;
 
 public class AirlinesTest {
 
     @Test
-    public void createAirlines(){
+    public void createAirlines() throws IOException {
 
-        Map<String,String> data= JSonUtils.getJSonDataAsMap("/src/test/resources/airlines/qa/")
-    String endPoint="https://api.instantwebtools.net/v1/airlines";
+        Map<String,String> data= JSonUtils.getJSonDataAsMap("airlines/qa/airlineApiData.json");
+         String endPoint=data.get("createAirlineEndpoint");
 
-    Map<String,Object> payload=Payload.getCreateAirlinePayloadFromMap("2","XYZ","India","https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
-            "From Sri Lanka","Katunayake, Sri Lanka","www.srilankaaairways.com","1992");
+    Map<String,Object> payload=Payload.getCreateAirlinePayloadFromMap("2","XYZ","India","https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png", "From Sri Lanka","Katunayake, Sri Lanka","www.srilankaaairways.com","1992");
     Response res= RestUtils.performPost(endPoint,payload, new HashMap<>());
         Assert.assertEquals(res.statusCode(),200);
 
